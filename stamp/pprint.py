d5b6366a9ca6a84842b1d2684b2be2a7bf466964
arivarton <accounts@arivarton.com>
2018-05-27 13:30:53 +0000

    Added a yes or no function to handle user input that requires a yes or no answer.

---
+++ stamp/pprint.py
@@ -0,0 +1,25 @@
+def yes_or_no(question,
+              no_message=None, no_function=None, no_function_args=(),
+              yes_message=None, yes_function=None, yes_function_args=()):
+    try:
+        user_choice = input(question + ' [Y/n]')
+    except KeyboardInterrupt:
+        user_choice = 'n'
+        if no_message:
+            no_message = '\n' + no_message
+
+    if user_choice.lower() in ['y', '']:
+        if yes_message:
+            print(yes_message)
+        if yes_function:
+            return yes_function(*yes_function_args)
+        else:
+            return None
+
+    else:
+        if no_message:
+            print(no_message)
+        if no_function:
+            return no_function(*no_function_args)
+        else:
+            return None
