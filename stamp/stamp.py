611c32ad0139e371ff010748c784078fad627c30
arivarton <accounts@arivarton.com>
2018-06-22 21:19:36 +0000

    Removed seconds and microseconds from default time. Closes #24

--- stamp/stamp.py
+++ stamp/stamp.py
@@ -155,7 +155,7 @@ def parse_args(args):
     date_parameters = argparse.ArgumentParser(add_help=False)
     date_parameters.add_argument('-D', '--date', type=lambda date: datetime.strptime(date, '%Y-%m-%d').date(), default=datetime.now().date(),
                                  help='Set date manually. Format is \'YYYY-mm-dd\'. Default is now.')
-    date_parameters.add_argument('-T', '--time', type=lambda time: datetime.strptime(time, '%H:%M').time(), default=datetime.now().time(),
+    date_parameters.add_argument('-T', '--time', type=lambda time: datetime.strptime(time, '%H:%M').time(), default=datetime.now().time().replace(second=0, microsecond=0),
                                  help='Set time manually. Format is \'HH:MM\'. Default is now.')
 
     # Filter parameters
