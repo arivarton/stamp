1457009c8b0364e57387fda53d28ca4dfc9d6788
arivarton <accounts@arivarton.com>
2018-06-30 12:11:49 +0000

    yes_or_no is now only taking a single character on input and automatically continuing without a newline. Closes #62

--- stamp/formatting.py
+++ stamp/formatting.py
@@ -1,34 +1,49 @@
 import sys
+import tty
+import termios
 from .helpers import get_terminal_width
 
 
-def yes_or_no(question,
-              no_message=None,
-              no_function=None, no_function_args=(), no_function_kwargs={},
-              yes_message=None,
-              yes_function=None, yes_function_args=(), yes_function_kwargs={}):
-    try:
-        print(question + ' [Y/n]')
-        user_choice = input()
-    except KeyboardInterrupt:
-        print('\nCanceling...')
-        sys.exit(0)
+def yes_or_no(question, no_message=None, no_function=None, no_function_args=(),
+              no_function_kwargs=False, yes_message=None, yes_function=None,
+              yes_function_args=(), yes_function_kwargs=False):
+    if not no_function_kwargs:
+        no_function_kwargs = {}
+    if not yes_function_kwargs:
+        yes_function_kwargs = {}
 
-    if user_choice.lower() in ['y', '']:
-        if yes_message:
-            print(yes_message)
-        if yes_function:
-            return yes_function(*yes_function_args, **yes_function_kwargs)
-        else:
-            return None
+    while True:
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
 
-    else:
-        if no_message:
-            print(no_message)
-        if no_function:
-            return no_function(*no_function_args, **no_function_kwargs)
+        try:
+            print(question + ' [Y/n]')
+            tty.setcbreak(fd)
+            user_choice = sys.stdin.read(1)
+        except KeyboardInterrupt:
+            print('\nCanceling...')
+            sys.exit(0)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+        if user_choice.lower() in ['y', '\n']:
+            if yes_message:
+                print(yes_message)
+            if yes_function:
+                return yes_function(*yes_function_args, **yes_function_kwargs)
+            else:
+                return None
+
+        elif user_choice.lower() in ['n']:
+            if no_message:
+                print(no_message)
+            if no_function:
+                return no_function(*no_function_args, **no_function_kwargs)
+            else:
+                return None
+
         else:
-            return None
+            print('\'%s\' is not a recognised answer!' % user_choice)
 
 
 def provide_input(provision_type):
