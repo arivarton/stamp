arivarton/stamp

commit fee25a4b3b73c063267dbb09b4e00b7f79622ac6

Redone the initialization of database sessions. Related to #57

stamp/db.py
@@ -13,31 +13,29 @@ from .settings import DATA_DIR
 from .formatting import yes_or_no
 
 
-def create_database(db_file):
-    engine = create_engine('sqlite:///' + db_file)
-    try:
-        Base.metadata.create_all(engine)
-    except exc.OperationalError as err:
-        if not os.path.exists(DATA_DIR):
-            os.makedirs(DATA_DIR)
-            Base.metadata.create_all(engine)
-        else:
-            raise err
-    return engine
 
 
 class Database():
     def __init__(self, db_file):
+        self.new_db = True
         if os.path.isfile(db_file):
-            engine = create_database(db_file)
+            engine = init_database(db_file)
         else:
-            engine = yes_or_no('Do you wish to create a new database called %s?' % db_file.split('/')[-1].split('.')[0],
-                               no_message='Canceled...',
-                               no_function=sys.exit,
-                               no_function_args=(0,),
-                               yes_message='Creating database!',
-                               yes_function=create_database,
-                               yes_function_args=(db_file,))
+            yes_or_no('Do you wish to create a new database called %s?' % db_file.split('/')[-1].split('.')[0],
+                      no_message='Canceled...',
+                      no_function=sys.exit,
+                      no_function_args=(0,),
+                      yes_message='Creating database!')
+            engine = create_engine('sqlite:///' + db_file)
+            try:
+                Base.metadata.create_all(engine)
+            except exc.OperationalError as err:
+                if not os.path.exists(DATA_DIR):
+                    os.makedirs(DATA_DIR)
+                    Base.metadata.create_all(engine)
+                else:
+                    raise err
+        return engine
         session = sessionmaker(bind=engine)
         self.session = session()
 

commit fca303f47fb4752946291f87fa4e0ad5cf09ed55

Fixed some bugs that were introduced in fee25a4b

stamp/db.py
@@ -18,25 +18,24 @@ from .formatting import yes_or_no
 class Database():
     def __init__(self, db_file):
         self.new_db = True
+        self.engine = create_engine('sqlite:///' + db_file)
         if os.path.isfile(db_file):
-            engine = init_database(db_file)
+            Base.metadata.create_all(self.engine)
         else:
             yes_or_no('Do you wish to create a new database called %s?' % db_file.split('/')[-1].split('.')[0],
                       no_message='Canceled...',
                       no_function=sys.exit,
                       no_function_args=(0,),
                       yes_message='Creating database!')
-            engine = create_engine('sqlite:///' + db_file)
             try:
-                Base.metadata.create_all(engine)
+                Base.metadata.create_all(self.engine)
             except exc.OperationalError as err:
                 if not os.path.exists(DATA_DIR):
                     os.makedirs(DATA_DIR)
-                    Base.metadata.create_all(engine)
+                    Base.metadata.create_all(self.engine)
                 else:
                     raise err
-        return engine
-        session = sessionmaker(bind=engine)
+        session = sessionmaker(bind=self.engine)
         self.session = session()
 
     def add(self, instance):

stamp/formatting.py
@@ -32,7 +32,10 @@ def yes_or_no(question,
         if user_choice.lower() in ['y', '\n']:
             if yes_message:
                 print(yes_message)
-            return yes_function(*yes_function_args, **yes_function_kwargs) or None
+            if yes_function:
+                return yes_function(*yes_function_args, **yes_function_kwargs)
+            else:
+                return None
 
         elif user_choice.lower() in ['n']:
             if no_message: