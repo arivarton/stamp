7a331b77418ca24d95d386978aee17514d6aab4a
arivarton <accounts@arivarton.com>
2018-10-19 20:08:26 +0000

    Closes #45.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(exec_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='stamp',
-    version='0.1.0',
+    version='0.1.4',
     description='Register workhours in terminal.',
     long_description=long_description,
     long_description_content_type='text/markdown',
